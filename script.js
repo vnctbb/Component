5a8ce6d08f13df3343475af7dfc02d517bb40508
Vincent <bongrand.v91@gmail.com>
2020-12-23 01:20:01 +0100

    multiple js files

--- script.js
+++ script.js
@@ -2,112 +2,40 @@
 
 window.addEventListener('DOMContentLoaded', function() {
 
-  const fAI = [
-    "orange.fr",
-    "outlook.fr",
-    "gmail.com",
-    "hotmail.fr",
-    "outlook.com",
-    "hotmail.com",
-    "wanadoo.fr",
-    "yahoo.fr",
-    "laposte.net",
-    "yahoo.com",
-    "sfr.fr",
-    "msn.com",
-    "live.fr",
-    "free.fr",
-    "numericable.fr",
-    "bbox.fr",
-    "neuf.fr"
-  ];
-
-// tableau contenant les trucs à afficher
-  let affiche = [
-    "orange.fr",
-    "outlook.fr",
-    "gmail.com",
-  ];
-
-// vérifie si il y'a le focus sur l'input
-  let focus = false;
-
-  function laFonction() {
-    if(myInput.value.indexOf('@') >= 0){
-      let c = 0;
-      let start = myInput.value.indexOf('@') + 1;
-      affiche = [];
-      fAI.forEach(function (a){
-        if(a.search(myInput.value.substring(start)) == 0){
-          affiche[c] = a;
-          c++;
-        }
-      });
-      if(affiche == 0){
-        affiche = [
-          "orange.fr",
-          "outlook.fr",
-          "gmail.com",
-        ];
-      }
-      if (!myInput.value.match(/[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/i)) {
-        console.log(myInput.value + " n'est pas une adresse valide");
-    } else {
-      console.log('adresse valide')
-    }
-    propose();
-    }
-  };
-
-  function propose () {
-    for(let i = 0; i < 3; i++){
-      if(!affiche[i]){
-        document.getElementsByClassName('prop')[i].innerHTML = '';
-      } else {
-        document.getElementsByClassName('prop')[i].innerHTML = affiche[i];
-      }
-    };
-  };
-
-  function set (index) {
-    if(myInput.value.indexOf('@') >= 0){
-      let start = myInput.value.indexOf('@') + 1;
-      myInput.value = myInput.value.substring(0, start) + affiche[index];
-      affiche = [];
-    } else {
-      if(myInput.value != ''){
-        myInput.value = `${myInput.value}@${affiche[index]}`;
-      }
-    }
-    propose();
-    myInput.focus();
-  };
-
+  // When the input is focus
   myInput.addEventListener('focus', function() {
-     propose();
+    // call change
+    change();
+    // set focus to true
     focus = true;
   });
 
+  // When the input isn't focus
   myInput.addEventListener('blur', function() {
+    // set focus to false
     focus = false;
   });
 
+  // When a key is tiped
   document.addEventListener('keyup', function(e) {
+    // if focus is true, and key is not Enter, and value is not empty
     if(focus && e.code != 'Enter' && myInput.value != ''){
-      laFonction();
+      // call checkInput()
+      checkInput();
     };
   });
 
+  // When a button is clicked, set the input's value
   document.getElementsByClassName('prop')[0].addEventListener('click', function(){
-    set(0);
-  });
-
-  document.getElementsByClassName('prop')[1].addEventListener('click', function(){
-    set(1);
+    setInput(0);
   });
   
+  document.getElementsByClassName('prop')[1].addEventListener('click', function(){
+    setInput(1);
+  });
+
   document.getElementsByClassName('prop')[2].addEventListener('click', function(){
-    set(2);
+    setInput(2);
   });
 });
 
